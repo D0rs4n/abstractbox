2caa7b58df1e9e79ee397679cb56cc70cdc4968a
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2022-08-26 01:56:14 +0200

    Implement JWT Auth middleware and add it to the application.

--- abstractbox/main.py
+++ abstractbox/main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from fastapi import FastAPI
 from models import HealthCheck
+from middleware import JWTAuthMiddleware
 
 app = FastAPI()
 
@@ -10,3 +11,5 @@ app = FastAPI()
 async def main() -> dict[str, str]:
     """A basic healthcheck endpoint, to assure the API daemon is up and running."""
     return {"timestamp": datetime.utcnow().isoformat()}
+
+app.add_middleware(JWTAuthMiddleware)
